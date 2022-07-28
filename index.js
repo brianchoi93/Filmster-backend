57904c2dd9293671098fafe0fe60aa118876989f
brianchoi93 <brianchoi93@gmail.com>
2022-07-27 17:00:14 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+//=============================================================================
+// Basic Config
+//=============================================================================
+// Import express
+const express = require('express');
+// Instantiate express
+const app = express();
+// Set server port
+app.set('port', process.env.PORT || 8000);
+
+//=============================================================================
+// Middleware
+//=============================================================================
+// Parse key value pairs in request
+app.use(express.urlencoded({ extended: true }));
+// Convert json strings to an object and attaches it to req.body
+app.use(express.json());
+// Use cors package to allow connections from all domains
+const cors = require('cors');
+app.use(cors());
+
+//=============================================================================
+// Routes
+//=============================================================================
+
+//=============================================================================
+// Start Controllers
+//=============================================================================
+
+//=============================================================================
+// End Controllers
+//=============================================================================
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).send(message);
+});
+//=============================================================================
+// Start Server
+//=============================================================================
+app.listen(app.get('port'), () => {
+	console.log(`✅ Listening on Port ${app.get('port')}`);
+});
